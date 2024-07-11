9259f9c7db765253f52c8e91443ce166c9cf6fac
Aman Yadav <amanyddv@gmail.com>
2024-07-11 12:33:33 +0530

    auth folder added

--- projects/admin/src/app/app.config.ts
+++ projects/admin/src/app/app.config.ts
@@ -11,5 +11,25 @@ import { getMessaging, provideMessaging } from '@angular/fire/messaging';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"fyi1-aa2c2","appId":"1:273891090725:web:452b2846870054e632bacc","storageBucket":"fyi1-aa2c2.appspot.com","apiKey":"AIzaSyA9mqYMpobF0WJuA0h-l1Qft5v7PjVwgOI","authDomain":"fyi1-aa2c2.firebaseapp.com","messagingSenderId":"273891090725","measurementId":"G-06JV06FSD9"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'fyi1-aa2c2',
+        appId: '1:273891090725:web:452b2846870054e632bacc',
+        storageBucket: 'fyi1-aa2c2.appspot.com',
+        apiKey: 'AIzaSyA9mqYMpobF0WJuA0h-l1Qft5v7PjVwgOI',
+        authDomain: 'fyi1-aa2c2.firebaseapp.com',
+        messagingSenderId: '273891090725',
+        measurementId: 'G-06JV06FSD9',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage()),
+  ],
 };
