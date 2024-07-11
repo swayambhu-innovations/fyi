9a95cb75f22a8cb6d20ab55ba6de1359ac7162a2
Aman Yadav <amanyddv@gmail.com>
2024-07-11 11:31:56 +0530

    firebase initialize

---
+++ projects/donor/src/app/app.config.ts
@@ -0,0 +1,15 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"fyi1-aa2c2","appId":"1:273891090725:web:1a3a2b61ba58a8e632bacc","storageBucket":"fyi1-aa2c2.appspot.com","apiKey":"AIzaSyA9mqYMpobF0WJuA0h-l1Qft5v7PjVwgOI","authDomain":"fyi1-aa2c2.firebaseapp.com","messagingSenderId":"273891090725","measurementId":"G-CJ026BL27H"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())]
+};
