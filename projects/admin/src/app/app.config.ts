c1aea188142fe5e2d6b7a74b3d697da3a031262b
Aman Yadav <amanyddv@gmail.com>
2024-07-11 11:44:07 +0530

    .

--- projects/admin/src/app/app.config.ts
+++ projects/admin/src/app/app.config.ts
@@ -3,7 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"fyi1-aa2c2","appId":"1:273891090725:web:452b2846870054e632bacc","storageBucket":"fyi1-aa2c2.appspot.com","apiKey":"AIzaSyA9mqYMpobF0WJuA0h-l1Qft5v7PjVwgOI","authDomain":"fyi1-aa2c2.firebaseapp.com","messagingSenderId":"273891090725","measurementId":"G-06JV06FSD9"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())]
 };
---
+++ projects/shared-ui/src/lib/shared-ui.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-shared-ui',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      shared-ui works!
+    </p>
+  `,
+  styles: ``
+})
+export class SharedUiComponent {
+
+}
