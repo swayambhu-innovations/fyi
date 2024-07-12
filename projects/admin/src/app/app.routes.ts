937618ef2690ae20fa759ce3658b75149c325d38
abhinav <resoabhisingh@gmail.com>
2024-07-12 12:25:08 +0530

    navbar-routing

--- projects/admin/src/app/app.routes.ts
+++ projects/admin/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { ServiceComponent } from './service/service.component';
+import { NavbarComponent } from './sharedComponent/navbar/navbar.component';
+import { ReportsComponent } from './reports/reports.component';
+import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
+import { FeedComponent } from './feed/feed.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component: NavbarComponent},
+    {path:'report',component: ReportsComponent},
+    {path:'feed',component: FeedComponent},
+    {path:'home',component: HomeComponent},
+    {path:'users',component: UsersComponent},
+    {path:'services',component: ServiceComponent},
+    
+];
