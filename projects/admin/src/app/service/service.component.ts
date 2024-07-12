6a8da19607c18a38621433d8a41dd23e0016f073
Aman Yadav <amanyddv@gmail.com>
2024-07-12 15:21:04 +0530

    menu header component communication

--- projects/admin/src/app/service/service.component.ts
+++ projects/admin/src/app/service/service.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderWithBackComponent } from '../sharedComponent/header-with-back/header-with-back.component';
 import { NavbarComponent } from '../sharedComponent/navbar/navbar.component';
 import { TaxComponent } from './tax/tax.component';
-import { HeaderWithMenuComponent } from "../sharedComponent/header-with-menu/header-with-menu.component"; 
-
+import {HeaderWithMenuComponent } from '../sharedComponent/header-with-menu/header-with-menu.component';
 @Component({
   selector: 'app-service',
   standalone: true,
-  imports: [HeaderWithBackComponent, NavbarComponent, TaxComponent, HeaderWithMenuComponent],
+  imports: [ NavbarComponent, TaxComponent, HeaderWithMenuComponent],
   templateUrl: './service.component.html',
   styleUrl: './service.component.scss'
 })
