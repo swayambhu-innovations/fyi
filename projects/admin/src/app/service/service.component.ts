aeb677be0b302eca570f91198743c21eec381b3b
Aman Yadav <amanyddv@gmail.com>
2024-07-12 14:55:48 +0530

    tax page created

--- projects/admin/src/app/service/service.component.ts
+++ projects/admin/src/app/service/service.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderWithBackComponent } from '../sharedComponent/header-with-back/header-with-back.component';
+import { NavbarComponent } from '../sharedComponent/navbar/navbar.component';
+import { TaxComponent } from './tax/tax.component'; 
 
 @Component({
   selector: 'app-service',
   standalone: true,
-  imports: [],
+  imports: [HeaderWithBackComponent,NavbarComponent,TaxComponent],
   templateUrl: './service.component.html',
   styleUrl: './service.component.scss'
 })
