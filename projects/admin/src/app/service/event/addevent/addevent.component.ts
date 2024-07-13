808dc448c968fbc551bbe187c7d677a51a84135f
abhinav <resoabhisingh@gmail.com>
2024-07-13 11:25:34 +0530

    .

--- projects/admin/src/app/service/event/addevent/addevent.component.ts
+++ projects/admin/src/app/service/event/addevent/addevent.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-addevent',
   standalone: true,
-  imports: [NgFor],
+  imports: [],
   templateUrl: './addevent.component.html',
   styleUrl: './addevent.component.scss'
 })
@@ -15,26 +14,5 @@ export class AddeventComponent {
   submit(select:string){
     this.selected=select;
   }
-  images: string[] = [];
-
-  removeImage(index: number): void {
-    this.images.splice(index, 1);
-  }
-  addImage(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    if (input.files) {
-      Array.from(input.files).forEach(file => {
-        const reader = new FileReader();
-        reader.onload = (e: any) => {
-          this.images.push(e.target.result);
-        };
-        reader.readAsDataURL(file);
-      });
-    }
-  }
-
-  triggerFileInput(): void {
-    const fileInput = document.getElementById('fileInput') as HTMLInputElement;
-    fileInput.click();
-  }
+  
 }
