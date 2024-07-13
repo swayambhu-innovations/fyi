717e1004a6b083f48b15a38a1aba64013aa0d7ee
abhinav <resoabhisingh@gmail.com>
2024-07-13 17:01:45 +0530

    add-event-ui

--- projects/admin/src/app/service/event/addevent/addevent.component.ts
+++ projects/admin/src/app/service/event/addevent/addevent.component.ts
@@ -1,18 +1,48 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { getDownloadURL, ref, uploadBytesResumable } from '@angular/fire/storage';
+import { Storage } from '@angular/fire/storage';
 @Component({
   selector: 'app-addevent',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './addevent.component.html',
   styleUrl: './addevent.component.scss'
 })
 export class AddeventComponent {
   selected=""
-  constructor(private router: Router) {}
+  constructor(private router: Router,private storage: Storage) {}
   submit(select:string){
     this.selected=select;
+  }
+
+  images: string[] = [];
+  uploadPercent: number = 0;
+
+  removeImage(index: number): void {
+    this.images.splice(index, 1);
+  }
+
+  async addImage(e: any) {
+    const input = e.target as HTMLInputElement;
+    if (input.files) {
+      Array.from(input.files).forEach(async file =>{
+        const filePath = `event/${new Date().getTime()}`;
+        console.log(filePath)
+        await uploadBytesResumable(ref(this.storage, filePath), file);
+        const fileUrl = await getDownloadURL(ref(this.storage, filePath));
+        this.images.push(fileUrl);
+      });
+    }
+    }
+ 
+  
+
+
+  triggerFileInput(): void {
+    const fileInput = document.getElementById('fileInput') as HTMLInputElement;
+    fileInput.click();
   }
   
 }
--- projects/admin/src/app/service/event/event.component.ts
+++ projects/admin/src/app/service/event/event.component.ts
@@ -1,24 +1,31 @@
 import { Component, ElementRef, Input, Renderer2, signal } from '@angular/core';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { NgFor } from '@angular/common';
+import { AddButtonComponent } from '../../../../../shared-ui/src/lib/add-button/add-button.component';
+import {MatBottomSheet,MatBottomSheetModule,} from '@angular/material/bottom-sheet';
+import {MatButtonModule} from '@angular/material/button';
 interface Event {
   name: string;
   imageUrl: string;
   booked: number;
   capacity: number;
   status: string;
-}import { AddeventComponent } from "./addevent/addevent.component";
+}
+
+import { AddeventComponent } from './addevent/addevent.component';
 
 @Component({
   selector: 'app-event',
   standalone: true,
-  imports: [MatExpansionModule,NgFor,AddeventComponent],
+  imports: [MatExpansionModule,NgFor,AddButtonComponent,MatButtonModule,
+    MatBottomSheetModule,NgFor],
   templateUrl: './event.component.html',
   styleUrl: './event.component.scss'
 })
 export class EventComponent {
   imageUrl: string | undefined
-  // const myElement = document.getElementById("demo");
+  constructor(private _bottomSheet: MatBottomSheet){}
+
 
   events: Event[] = [
     {
@@ -63,15 +70,8 @@ export class EventComponent {
   deleteEvent(event: Event): void {
     console.log('Deleting event', event);
   }
-  // image: string ='/assets/login/even_bg.svg';
-  // @Input() imageUrl!: string;
-  //   constructor(private elementRef:ElementRef, private renderer: Renderer2) {}
-  //   ngOnInit() {
-  //       this.renderer.setStyle(
-  //           this.elementRef.nativeElement,
-  //           'backgroundImage',
-  //           `url(${this.imageUrl})`
-  //       );
-  //   }
+  openBottomSheet(): void {
+    this._bottomSheet.open(AddeventComponent);
+  }
 
 }
