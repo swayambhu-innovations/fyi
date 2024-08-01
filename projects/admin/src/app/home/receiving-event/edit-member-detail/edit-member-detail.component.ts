c6dc18b87d519762593d767fed1fb8718c859b56
Aman Yadav <amanyddv@gmail.com>
2024-08-01 12:47:47 +0530

    prefilled data in members form in booking tab in admin app

--- projects/admin/src/app/home/receiving-event/edit-member-detail/edit-member-detail.component.ts
+++ projects/admin/src/app/home/receiving-event/edit-member-detail/edit-member-detail.component.ts
@@ -1,10 +1,13 @@
 import { Component, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import {
+  MAT_BOTTOM_SHEET_DATA,
+  MatBottomSheetRef,
+} from '@angular/material/bottom-sheet';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CancelBtnComponent } from "../../../../../../shared-ui/src/cancel-btn/cancel-btn.component";
-import { SaveBtnComponent } from "../../../../../../shared-ui/src/save-btn/save-btn.component";
+import { CancelBtnComponent } from '../../../../../../shared-ui/src/cancel-btn/cancel-btn.component';
+import { SaveBtnComponent } from '../../../../../../shared-ui/src/save-btn/save-btn.component';
 interface Member {
   name: string;
   aadhaar: string;
@@ -14,9 +17,14 @@ interface Member {
 @Component({
   selector: 'app-edit-member-detail',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, CancelBtnComponent, SaveBtnComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    CancelBtnComponent,
+    SaveBtnComponent,
+  ],
   templateUrl: './edit-member-detail.component.html',
-  styleUrl: './edit-member-detail.component.scss'
+  styleUrl: './edit-member-detail.component.scss',
 })
 export class EditMemberDetailComponent {
   editMemberForm: FormGroup;
@@ -24,13 +32,19 @@ export class EditMemberDetailComponent {
   constructor(
     private fb: FormBuilder,
     private bottomSheetRef: MatBottomSheetRef<EditMemberDetailComponent>,
-    @Inject(MAT_BOTTOM_SHEET_DATA) public member: Member
+    @Inject(MAT_BOTTOM_SHEET_DATA) public member: any
   ) {
     this.editMemberForm = this.fb.group({
-      name: [this.member.name, Validators.required],
-      aadhaar: [this.member.aadhaar, [Validators.required, Validators.pattern(/^\d{12}$/)]],
-      gender: [this.member.gender, Validators.required],
-      contact: [this.member.contact, [Validators.required, Validators.pattern(/^\d{10}$/)]]
+      name: [member.name, Validators.required],
+      aadhaar: [
+        member?.['Aadhar'],
+        [Validators.required, Validators.pattern(/^\d{12}$/)],
+      ],
+      gender: [member?.['Gender'], Validators.required],
+      contact: [
+        member?.['Contact'],
+        [Validators.required, Validators.pattern(/^\d{10}$/)],
+      ],
     });
   }
 
