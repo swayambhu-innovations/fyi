d682f42022cf5d5b165b0cfbd17deb48864c7669
anujkesharwani <anujkesarwani31@gmail.com>
2024-08-02 12:14:33 +0530

    UI of admin done

--- projects/admin/src/app/users/users.component.ts
+++ projects/admin/src/app/users/users.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import {HeaderWithMenuComponent } from '../sharedComponent/header-with-menu/header-with-menu.component';
+import { HeaderWithBackComponent } from "../sharedComponent/header-with-back/header-with-back.component";
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [HeaderWithMenuComponent],
+  imports: [HeaderWithMenuComponent, HeaderWithBackComponent],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
 export class UsersComponent {
-
 }
