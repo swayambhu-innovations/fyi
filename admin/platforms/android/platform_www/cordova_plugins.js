ea255d8acf9ae6dd49531be470e98e9df5745083
Aman Yadav <amanyddv@gmail.com>
2024-08-12 15:22:13 +0530

    suvery tab created

---
+++ admin/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,24 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-firebasex.FirebasePlugin",
+      "file": "plugins/cordova-plugin-firebasex/www/firebase.js",
+      "pluginId": "cordova-plugin-firebasex",
+      "clobbers": [
+        "FirebasePlugin"
+      ]
+    },
+    {
+      "id": "cordova-plugin-googleplus.GooglePlus",
+      "file": "plugins/cordova-plugin-googleplus/www/GooglePlus.js",
+      "pluginId": "cordova-plugin-googleplus",
+      "clobbers": [
+        "window.plugins.googleplus"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-firebasex": "16.5.0",
+    "cordova-plugin-googleplus": "8.5.2"
+  };
+});
--- projects/admin/src/app/survey-form/survey-form.component.ts
+++ projects/admin/src/app/survey-form/survey-form.component.ts
@@ -1,13 +1,51 @@
 import { Component } from '@angular/core';
-import { HeaderWithMenuComponent } from "../sharedComponent/header-with-menu/header-with-menu.component";
-
+import { HeaderWithBackComponent } from '../sharedComponent/header-with-back/header-with-back.component';
+import { AddButtonComponent } from '../../../../shared-ui/src/lib/add-button/add-button.component';
+import { AddSurveyComponent } from './add-survey/add-survey.component';
+import { LoadingService } from '../../../../shared-ui/src/lib/spinner/loading.service';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { GlobalServiceService } from '../globalService/global-service.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-survey-form',
   standalone: true,
-  imports: [HeaderWithMenuComponent],
+  imports: [HeaderWithBackComponent, AddButtonComponent, CommonModule],
   templateUrl: './survey-form.component.html',
-  styleUrl: './survey-form.component.scss'
+  styleUrl: './survey-form.component.scss',
 })
 export class SurveyFormComponent {
+  constructor(
+    private _bottomSheet: MatBottomSheet,
+    private loadingService: LoadingService,
+    private GlobalService: GlobalServiceService,
+  ) {}
 
+  surveyList: any = [];
+
+  ngOnInit() {
+    try {
+      this.GlobalService.fetchDocs('surveys').subscribe((surveys) => {
+        this.surveyList = surveys;
+        console.log(this.surveyList);
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  addSurvey(): void {
+    this._bottomSheet.open(AddSurveyComponent);
+  }
+  edit(survey:any) {
+    console.log(survey)
+    // this._bottomSheet.open(AddSurveyComponent, {
+    //   data: survey,
+    // });
+  }
+  delete(survey:any) {
+    console.log(survey)
+    //this.GlobalService.deleteDoc('surveys', survey.id);
+  }
+  updatedStatus(survey:any){
+    console.log(survey)
+  }
 }
