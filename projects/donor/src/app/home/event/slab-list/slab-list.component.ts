0bfeab1bcb70fda7e29717f73d00233c734f5dce
anujkesharwani <anujkesarwani31@gmail.com>
2024-08-17 10:58:21 +0530

    bug fixed

--- projects/donor/src/app/home/event/slab-list/slab-list.component.ts
+++ projects/donor/src/app/home/event/slab-list/slab-list.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HeaderWithBackComponent } from '../../../sharedComponent/header-with-back/header-with-back.component';
 import { EventService } from '../event.service';
+import { LoadingService } from '../../../../../../shared-ui/src/lib/spinner/loading.service';
 @Component({
   selector: 'app-slab-list',
   standalone: true,
@@ -14,7 +15,8 @@ export class SlabListComponent {
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
-    public EventService: EventService
+    public EventService: EventService,
+    private LoadingService:LoadingService
   ) {
     this.activatedRoute.paramMap.subscribe(async (params) => {
       if (params.get('id') !== null) {
@@ -26,29 +28,40 @@ export class SlabListComponent {
     }
   }
   eventId: any;
-  items = [
-    {
-      image: '/assets/homepage/slabimage.svg',
-      title: 'Panchganga Slab',
-    },
-    {
-      image: '/assets/homepage/slabimage.svg',
-      title: 'Assi Slab',
-    },
-    {
-      image: '/assets/homepage/slabimage.svg',
-      title: 'Panchganga Slab',
-    },
-    {
-      image: '/assets/homepage/slabimage.svg',
-      title: 'Assi Slab',
-    },
-  ];
+  // items = [
+  //   {
+  //     image: '/assets/homepage/slabimage.svg',
+  //     title: 'Panchganga Slab',
+  //   },
+  //   {
+  //     image: '/assets/homepage/slabimage.svg',
+  //     title: 'Assi Slab',
+  //   },
+  //   {
+  //     image: '/assets/homepage/slabimage.svg',
+  //     title: 'Panchganga Slab',
+  //   },
+  //   {
+  //     image: '/assets/homepage/slabimage.svg',
+  //     title: 'Assi Slab',
+  //   },
+  // ];
 
   ngOnInit(){
+    this.LoadingService.show();
+    this.loadSlabs().then(() => {
+      this.LoadingService.hide();
+    });
   
   }
-
+  async loadSlabs() {
+    // Simulate a delay for loading slabs, replace with actual logic
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(true);
+      }, 1000);
+    });
+  }
   movetovarient(slabId: any,slab:any) {
     let bookingDetails =this.EventService.bookingDetails()
     bookingDetails['slab'] = slab;
